b7a71608094d84e41927be9a744664e8e8d88dc2
alzemiro-iago <alzemiro.iago@zoly.com.br>
2021-05-14 10:30:07 -0300

    "Reset repo"

--- packages/apps/tooltip-component/src/direflow-components/tooltip-component/WebComponents/HeadForm.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import HeadFormComponent from "../components/head-form"
-
-const HeadFormWebComponent = () => {
-  return (<HeadFormComponent />)
-}
-export default HeadFormWebComponent
--- packages/apps/tooltip-component/src/direflow-components/tooltip-component/WebComponents/TolltipGatilho.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import TooltipGatilho from "../components/tooltip-gatilho"
-
-const TooltipGatilhoComponent = () => {
-  return (<TooltipGatilho />)
-}
-export default TooltipGatilhoComponent

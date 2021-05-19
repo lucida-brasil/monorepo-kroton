2f0cc69f105eb61c9d25ad358654ae51a1c94f77
alzemiro-iago <alzemiro.iago@zoly.com.br>
2021-05-19 10:51:03 -0300

    teste deploy

--- packages/anhanguera-tooltip/src/index.js
+++ packages/anhanguera-tooltip/src/index.js
@@ -1,12 +1,30 @@
 import { HTMLActions } from '@zoly-cro/hooks'
+import HeadForm from './direflow-components/tooltip-component/components/head-form'
 
-window.addEventListener('load', () => {
-  // const components = document.querySelectorAll('.fnl-input-wrapper')
-  // if (!components) return
-  // const cpf = document.querySelector('.fnl-input-wrapper #cpf').parentElement
-  // const rg = document.querySelector('.fnl-input-wrapper #rg').parentElement
-  
-  // HTMLActions.hideElement(cpf)
-  // HTMLActions.hideElement(rg)
 
-})
+function mainFunction() {
+  try {
+    const components = document.querySelectorAll('.fnl-input-wrapper')
+    if (components === undefined) return
+    const cpf = document.querySelector('.fnl-input-wrapper #cpf').parentElement
+    const rg = document.querySelector('.fnl-input-wrapper #rg').parentElement
+    const dtNascimento = document.querySelector('.fnl-input-wrapper #dataNascimento').parentElement
+    const anoConclusao = document.querySelector('.fnl-select-search #fnl-dataconclusao-search-input-div').parentElement
+    const elements = [
+      cpf,
+      rg,
+      dtNascimento,
+      anoConclusao,
+      document.querySelector('.fnl-flex-row #fnl-section-opcoes-vestibular').parentElement,
+      document.querySelector('#zoly-section-endereco'),
+      document.querySelector('#zoly-section-acessibilidade')
+    ]
+    
+    HTMLActions.hideElements(elements)
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+window.addEventListener('loadedmetadata', mainFunction())
+
